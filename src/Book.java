b1c83ab14564af69a1b79c524b02e3f8cb3aef64
Ankita Kundu <ankita.2020.kundu@gmail.com>
2026-03-22 02:03:13 +0530

    Fixed syntactical errors

--- src/Book.java
+++ src/Book.java
@@ -2,7 +2,8 @@
 
 import java.io.Serializable;
 
-public class Book implements Serializable {
+public class Book implements Serializable 
+{
     private static final long serialVersionUID = 1L;
 
     private int id;
@@ -14,7 +15,8 @@ public class Book implements Serializable {
     private double price;
     private boolean available;
 
-    public Book(int id, String title, String author, String genre, String isbn, int quantity, double price) {
+    public Book(int id, String title, String author, String genre, String isbn, int quantity, double price) 
+    {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -25,32 +27,82 @@ public class Book implements Serializable {
         this.available = quantity > 0;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() 
+    { 
+     return id;
+     }
+    public void setId(int id) 
+    { 
+        this.id = id; 
+    }
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public String getTitle() 
+    { 
+        return title;
+     }
+    public void setTitle(String title)
+     { 
+        this.title = title; 
+    }
 
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
+    public String getAuthor() 
+    { 
+        return author; 
+    }
+    public void setAuthor(String author) 
+    { 
+        this.author = author; 
+    }
 
-    public String getGenre() { return genre; }
-    public void setGenre(String genre) { this.genre = genre; }
+    public String getGenre()
+     { 
+        return genre; 
+    }
+    public void setGenre(String genre)
+     { 
+        this.genre = genre; 
+    }
 
-    public String getIsbn() { return isbn; }
-    public void setIsbn(String isbn) { this.isbn = isbn; }
+    public String getIsbn() 
+    { 
+        return isbn; 
+    }
+    public void setIsbn(String isbn)
+     { 
+        this.isbn = isbn; 
+    }
 
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public int getQuantity()
+     { 
+        return quantity; 
+    }
+    public void setQuantity(int quantity) 
+    { 
+        this.quantity = quantity;
+     }
 
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
+    public double getPrice() 
+    { return price; 
 
-    public boolean isAvailable() { return available; }
-    public void setAvailable(boolean available) { this.available = available; }
+    }
+    public void setPrice(double price)
+     { 
+        this.price = price;
+     }
+
+    public boolean isAvailable() 
+    { return available; 
+
+    }
+    public void setAvailable(boolean available)
+     { 
+        
+    this.available = available;
+ }
 
     @Override
-    public String toString() {
+    public String toString()
+     {
         return "Book{id=" + id + ", title='" + title + "', author='" + author + "'}";
     }
 }
